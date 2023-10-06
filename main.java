815c6654c9ba02fa795553e242b287015e312c53
alanyur <alanibagon@gmail.com>
2023-10-06 12:18:36 -0300

    prueba main

--- main.java
+++ main.java
@@ -13,6 +13,7 @@ public class main {
         Articulo Lapiz = new Articulo(200,"Lapiz Pasta", "Descripcion1",1000);
         Articulo LapizMina = new Articulo(300,"Lapiz Mina", "Descripcion2",900);
         Articulo Goma = new Articulo(200,"Goma", "Descripcion3",500);
+        Articulo Estuche = new Articulo(500, "Estuche", "Descripcion4",3000);
         //creacion de los detalles
         DetalleOrden D1 = new DetalleOrden(300,Lapiz);
         Lapiz.addDetalleOrden(D1);
@@ -20,11 +21,13 @@ public class main {
         LapizMina.addDetalleOrden(D2);
         DetalleOrden D3 = new DetalleOrden(100, Goma);
         Goma.addDetalleOrden(D3);
+        DetalleOrden D4 = new DetalleOrden(50, Estuche);
         // Orden de compra
         OrdenCompra orden1 = new OrdenCompra(fecha1,"comprado");
         orden1.addDetalle(D1);
         orden1.addDetalle(D2);
         orden1.addDetalle(D3);
+        orden1.addDetalle(D4);
 
         System.out.println(cliente1.toString());
 
